c7f1e3f8d794cde22549b599e1547d3f89a6d8b9
maxrexfax <maxrexfax@gmail.com>
2021-04-16 22:33:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <header>
+        <img src="https://cdn.dribbble.com/users/31864/screenshots/3666062/attachments/819877/2.jpg?compress=1&resize=400x300"/>
+      </header>
+      <nav>
+        Profile
+      </nav>
+      <div>
+        Main content
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function Welcome() {
+	return <h1>Welcome, Welcome!</h1>;
+}
+export default Welcome;
